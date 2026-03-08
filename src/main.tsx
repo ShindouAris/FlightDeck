c676c3d9a5d667560f673a4e34a778a5fcebb879
Chisadin <tranthanhnam.ccc@gmail.com>
2026-03-08 22:14:26 +0700

    feat: add @vercel/analytics dependency and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import App from './App.tsx'
 import { GooeyToaster } from 'goey-toast'
 import "goey-toast/styles.css"
 import './i18n.ts'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <GooeyToaster position="top-center" />
     <App />
   </StrictMode>,
